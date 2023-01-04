41235f96ee2c0b64b988f91d6587cd0e5861a292
hadi-16 <rockers.screamo@yahoo.co.id>
2023-01-04 21:46:10 +0700

    add chat store

---
+++ store/ChatStore.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+// initial chat store
+export const ChatStore = create(
+  persist(
+    (set, get) => ({
+      // default array chats
+      chats: [],
+      // default loading false
+      loading: false,
+      // store chat to api
+      addChat: async (inputChat) => {
+        try {
+          // set loading true
+          set(() => ({ loading: true }));
+          // post data input to api
+          const { data } = await axios.post("/api/chat", {
+            chat: inputChat,
+          });
+          // data chat object
+          const dataChat = {
+            // input chat
+            chat: inputChat,
+            // answer from api
+            answer: data,
+            // current date
+            date: new Date(),
+          };
+          // set new data chat from api to new array
+          set((state) => ({
+            chats: [...state.chats, dataChat],
+            loading: false,
+          }));
+        } catch (err) {
+          console.log(err);
+          set(() => ({ loading: false }));
+        }
+      },
+      // remove one chat
+      removeOneChat: (item) =>
+        // remove one chat by index
+        set((state) => ({
+          chats: state.chats.filter((x) => x !== item),
+        })),
+      // remove all chats
+      removeAllChat: () => set({ chats: [] }),
+    }),
+    // set local storage
+    {
+      name: "next-openai-chats",
+      getStorage: () => localStorage,
+    }
+  )
+);
