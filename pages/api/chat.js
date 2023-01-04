863853ac89b3b79c4c3a9b37ac8f4a0214b1a6e8
hadi-16 <rockers.screamo@yahoo.co.id>
2023-01-04 21:48:10 +0700

    add rest api chat

---
+++ pages/api/chat.js
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { Configuration, OpenAIApi } = require("openai");
+
+export default async function handler(req, res) {
+  // if method not post send this
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed!!" });
+    return;
+  }
+  // if method post then do it
+  if (req.method === "POST") {
+    // req body with name is chat
+    const chat = req.body.chat;
+    // if there is a chat
+    if (chat) {
+      // configuration api openai
+      const configuration = new Configuration({
+        apiKey: process.env.API_KEY,
+      });
+      const openai = new OpenAIApi(configuration);
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: chat,
+        temperature: 0,
+        max_tokens: 500,
+      });
+      // if get data from openai send json
+      if (response.data?.choices) {
+        res.json(response.data.choices[0].text);
+        // if can't get data from openai send error
+      } else {
+        res.status(500).send("Oops, Something went wrong!!");
+      }
+      // if no chat send error not found
+    } else {
+      res.status(404).send("Please, write your chat!!");
+    }
+  }
+}
